ee37849110fa02e21d942680f1276a2db69deae2
Leidy Alvarez <leidyalvares32@gmail.com>
2026-09-02 04:17:58 -0500

    Add generic repository, match repository and database seeder (Module 3)

--- Golbet.Web/obj/Debug/net8.0/Golbet.Web.AssemblyInfo.cs
+++ Golbet.Web/obj/Debug/net8.0/Golbet.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Golbet.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0279bbc98661a24a4e7106c00fe747b28250bb7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ea326a4b9048de9829374463a3790c1d31cd6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Golbet.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Golbet.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
